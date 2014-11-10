b4681da262a6158eb69c2b0f03f1529136cee92f
gerferra <german.ferrari@gmail.com>
2014-11-10 20:33:22 -0200

    Revert para limpiar pull request

--- src/main/scala/Duplicates.scala
+++ src/main/scala/Duplicates.scala
@@ -5,49 +5,14 @@
   * FIXME add implementations
   *
   */
-
-import scala.io.Source
-
 class Duplicates(val fileName: String) {
 
-  val SongRegex = """\d+\s+(\d+)\s+\d+\s+[^\s].*""".r
+  def lines: Seq[String] = ???
 
-  def linesWithId: Seq[(Long, String)] = {
-    lines.collect { 
-        case line @ SongRegex(songId) => songId.toLong -> line
-      }
-  }
+  def allSongs: Seq[String] = ???
 
-  def lines: Seq[String] = Source.fromFile(fileName).getLines.toSeq
+  def uniqueSongs: Seq[String] = ???
 
-  def allSongs: Seq[String] = linesWithId.unzip._2
-
-  def uniqueSongs: Seq[String] = {
-    val uniqueSongLines = linesWithId.groupBy(_._1).filter(_._2.size == 1).toSeq
-    
-    val res =
-      for {
-        (songId, lines) <- uniqueSongLines
-        (_, uniqueLine) = lines.head
-      } yield {
-        uniqueLine
-      }
-
-    res
-  }
-
-  def duplicateSongs: Seq[String] = {
-    val dupSongLines = linesWithId.groupBy(_._1).filter(_._2.size >= 2).toSeq
-    
-    val res =
-      for {
-        (songId, lines) <- dupSongLines
-        (_, line) <- lines
-      } yield {
-        line
-      }
-
-    res
-  }
+  def duplicateSongs: Seq[String] = ???
 
 }
