ce54b190bf70abed3d1f6e77a037587b95029e8a
gerferra <german.ferrari@gmail.com>
2014-11-10 22:31:18 -0200

    Solución usando regex

--- src/main/scala/Duplicates.scala
+++ src/main/scala/Duplicates.scala
@@ -5,14 +5,49 @@
   * FIXME add implementations
   *
   */
+
+import scala.io.Source
+
 class Duplicates(val fileName: String) {
 
-  def lines: Seq[String] = ???
+  val SongRegex = """\d+\s+(\d+)\s+\d+\s+[^\s].*""".r
 
-  def allSongs: Seq[String] = ???
+  def lines: Seq[String] = Source.fromFile(fileName).getLines.toSeq
 
-  def distinctSongs: Seq[String] = ???
+  def linesWithId: Seq[(Long, String)] = {
+    lines.collect { 
+        case line @ SongRegex(songId) => songId.toLong -> line
+      }
+  }
 
-  def duplicateSongs: Seq[String] = ???
+  def allSongs: Seq[String] = linesWithId.unzip._2
+
+  def distinctSongs: Seq[String] = {
+    val distinctSongs = linesWithId.groupBy(_._1).toSeq
+    
+    val res =
+      for {
+        (songId, lines) <- distinctSongs
+        (_, repLine) = lines.head
+      } yield {
+        repLine
+      }
+
+    res
+  }
+
+  def duplicateSongs: Seq[String] = {
+    val dupSongLines = linesWithId.groupBy(_._1).filter(_._2.size >= 2).toSeq
+    
+    val res =
+      for {
+        (songId, lines) <- dupSongLines
+        (_, repLine) = lines.head
+      } yield {
+        repLine
+      }
+
+    res
+  }
 
 }
