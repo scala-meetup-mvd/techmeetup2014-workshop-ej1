ee54d2df65f9967201f16fdb0f2e066e6e32da58
Guillermo Polito <guillepol@gmail.com>
2014-11-11 10:30:56 -0200

    adjust the songId type from String to Long.

--- src/main/scala/Duplicates.scala
+++ src/main/scala/Duplicates.scala
@@ -12,7 +12,7 @@ class Duplicates(val fileName: String) {
   val songLineRegEx = """(\d+)\s+(\d+)\s+(\d+)(.*)""".r
 
   val allSongIds = allSongs.map {
-      case songLineRegEx(_, songId, _*) => songId
+      case songLineRegEx(_, songId, _*) => songId.toLong
   }
 
   def lines: Seq[String] = Source.fromFile(fileName).getLines.toSeq
@@ -22,19 +22,19 @@ class Duplicates(val fileName: String) {
       case _ => false
     }
 
-  def distinctSongs: Seq[String] = allSongIds
+  def distinctSongs: Seq[Long] = allSongIds
     .groupBy(s => s)
     .keys
     .toSeq
 
-  def duplicateSongs: Seq[String] = allSongIds
+  def duplicateSongs: Seq[Long] = allSongIds
     .groupBy(s => s)
     .mapValues(_.size)
     .filter {case (song, count) => count > 1}
     .keys
     .toSeq
 
-  def duplicateSongsWithCount: Map[String,Int] = allSongIds
+  def duplicateSongsWithCount: Map[Long,Int] = allSongIds
     .groupBy(s => s)
     .mapValues(_.size)
 
--- src/test/scala/DuplicatesTest.scala
+++ src/test/scala/DuplicatesTest.scala
@@ -9,7 +9,7 @@ class DuplicatesTest extends BetterFunSuite  {
   val allLinesCount     = 129
   val allSongsCount     = 100
   val uniqueSongsCount  = 94
-  val duplicateSongsIdCountMap = Map("137802" -> 2, "216869" -> 2, "190961" -> 2, "130280" -> 2, "113955" -> 2, "257560" -> 2)
+  val duplicateSongsIdCountMap = Map(137802 -> 2, 216869 -> 2, 190961 -> 2, 130280 -> 2, 113955 -> 2, 257560 -> 2)
 
   "Duplicados" should "be able to read all lines" in {
     val dupes = new Duplicates(relativeName)
